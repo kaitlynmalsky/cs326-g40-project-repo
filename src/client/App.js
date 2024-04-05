6acb5914cfbad8d0d1b5a7cac574405c543088a4
kaitlynmalsky <93287571+kaitlynmalsky@users.noreply.github.com>
2024-04-05 12:00:19 -0400

    fragmented mapView

--- src/client/App.js
+++ src/client/App.js
@@ -10,16 +10,10 @@ export class App {
     const rootElm = document.getElementById(root);
     rootElm.innerHTML = '';
 
-    const mapElement = document.createElement('div');
-    mapElement.id = 'map';
 
-    rootElm.appendChild(mapElement);
-    let map = L.map('map').setView([42.3868, -72.5293], 17);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-    
+    const mapView = new MapView();
+    rootElm.appendChild(await mapView.render());
+    await mapView.setView(42.3868, -72.5293, 17);
     
   }
 
--- src/client/Map/MapView.js
+++ src/client/Map/MapView.js
@@ -1,3 +1,21 @@
 export class MapView {
+    #map = null;
+    
+    constructor() {
 
+    }
+
+    async render() {
+        const elm = document.createElement('div');
+        elm.id = 'map';
+        return elm;
+    }
+
+    async setView(x, y, zoom) {
+        let map = L.map('map').setView([x, y], zoom);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    }
 }
