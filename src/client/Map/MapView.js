f0875c06daf3ac345a3e383e97fc09bc7a62a046
kaitlynmalsky <93287571+kaitlynmalsky@users.noreply.github.com>
2024-04-07 11:47:21 -0400

    starter marker code + shadow placeholder image

--- src/client/Map/MapView.js
+++ src/client/Map/MapView.js
@@ -12,10 +12,27 @@ export class MapView {
     }
 
     async setView(x, y, zoom) {
-        let map = L.map('map').setView([x, y], zoom);
+        this.#map = L.map('map').setView([x, y], zoom);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+        }).addTo(this.#map);
+    }
+
+    createIcon(imageLink, shadowLink) {
+        return L.icon({
+            iconUrl: imageLink,
+            shadowUrl: shadowLink,
+
+            iconSize:     [38, 95], // size of the icon
+            shadowSize:   [50, 64], // size of the shadow
+            iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+            shadowAnchor: [4, 62],  // the same for the shadow
+            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
+    }
+
+    createMarker(x, y, icon) {
+        L.marker([x, y], {icon: icon}).addTo(this.#map);
     }
 }
