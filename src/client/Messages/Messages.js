f718d38ff5371fd076cabd38313c1fed2ae98980
kaitlynmalsky <93287571+kaitlynmalsky@users.noreply.github.com>
2024-04-13 16:02:58 -0400

    message split prelim

--- src/client/Messages/Messages.js
+++ src/client/Messages/Messages.js
@@ -6,9 +6,24 @@ export default class MessagesView extends View {
     }
 
     async render() {
-        const elm = document.createElement('div');
+        const elm = document.createElement('header');
         elm.id = 'messages-view';
-        elm.innerText = "message view";
+
+        const splitScreenSection = document.createElement('section');
+        const splitGrid = document.createElement('div');
+        splitGrid.className = "grid h-screen grid-cols-2 gap-10";
+        const col1 = document.createElement('div');
+        col1.innerHTML = "hi"
+        const col2 = document.createElement('div');
+        col2.innerHTML = "bye";
+        col1.classList.add("bg-amber-500");
+
+
+        elm.appendChild(splitScreenSection);
+        splitScreenSection.appendChild(splitGrid);
+        splitGrid.appendChild(col1);
+        splitGrid.appendChild(col2);
+
         return elm;
     }
 
