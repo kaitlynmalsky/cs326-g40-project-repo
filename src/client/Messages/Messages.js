0c842095f63d757c0d26d172d4f6c1665e339cf7
kaitlynmalsky <93287571+kaitlynmalsky@users.noreply.github.com>
2024-04-14 16:46:40 -0400

    text box for messageview

--- src/client/Messages/Messages.js
+++ src/client/Messages/Messages.js
@@ -7,6 +7,12 @@ export default class MessagesView extends View {
     #col2;
     #chatView;
     #sendView;
+    #currUser;
+
+    /**
+     * Creates the message view (no parameters).
+     * @constructor
+     */
     
     constructor() {
         super();
@@ -15,6 +21,10 @@ export default class MessagesView extends View {
         
     }
 
+    /**
+     * 
+     * @returns 
+     */
     async render() {
         const elm = document.createElement('header');
         elm.id = 'messages-view';
@@ -27,15 +37,18 @@ export default class MessagesView extends View {
         this.#col1.id = "col1";
 
         this.#col2 = document.createElement('div');
-        this.#col2.className = "grid grid-rows-2 col-span-4"
+        this.#col2.className = "grid grid-rows-5 col-span-4 overflow-y-scroll"
         this.#col2.id = "col2";
         this.#chatView = document.createElement('div');
-        this.#chatView.className = "max-h-screen overflow-y-scroll message-view-col gap-2.5 grid grid-flow-row auto-rows-max";
+        this.#chatView.className = "overflow-y-scroll gap-2.5 grid grid-flow-row auto-rows-max row-span-4";
         this.#col2.appendChild(this.#chatView);
 
         this.#sendView = document.createElement('div');
+        this.#sendView.className = "p-5";
         this.#sendView.id = "send-message";
         this.#col2.appendChild(this.#sendView);
+
+        this.initializeSendView();
 
         // const remainingBar = document.createElement('div');
         // remainingBar.style = "flex: 1 1 auto;"
@@ -62,6 +75,10 @@ export default class MessagesView extends View {
     }
 
     demoMessages() {
+        this.#currUser = {
+            name: "Cool Cat",
+            avatar: "../images/placeholder_avatar.png"
+        }
         const spiderman = {
             name: "Spiderman",
             avatar: "../images/spiderman.png"
@@ -115,6 +132,7 @@ export default class MessagesView extends View {
         
         End communication.
         `);
+        this.addMessage(spiderman, Date.now(), `Today when I walked into my economics class I saw something I dread every time I close my eyes. Someone had brought their new gaming laptop to class. The Forklift he used to bring it was still running idle at the back. I started sweating as I sat down and gazed over at the 700lb beast that was his laptop. He had already reinforced his desk with steel support beams and was in the process of finding an outlet for a power cable thicker than Amy Schumer's thigh. I start shaking. I keep telling myself I'm going to be alright and that there's nothing to worry about. He somehow finds a fucking outlet. Tears are running down my cheeks as I send my last texts to my family saying I love them. The teacher starts the lecture, and the student turns his laptop on. The colored lights on his RGB Backlit keyboard flare to life like a nuclear flash, and a deep humming fills my ears and shakes my very soul. The entire city power grid goes dark. The classroom begins to shake as the massive fans begin to spin. In mere seconds my world has gone from vibrant life, to a dark, earth shattering void where my body is getting torn apart by the 150mph gale force winds and the 500 decibel groan of the cooling fans. As my body finally surrenders, I weep, as my school and my city go under. I fucking hate gaming laptops. `);
         
     }
 
@@ -149,14 +167,14 @@ export default class MessagesView extends View {
         this.#col1.appendChild(gcElm);
     }
 
-    addMessage(person, timestamp, message) {
+    addMessage(person, timestamp, message, fromMe) {
         const messageElm = document.createElement('div');
         messageElm.className = "flex";
 
         const messageAvatar = document.createElement('img');
         messageAvatar.className = " w-20 h-20 m-3 flex-none border-2 border-white rounded-full"
         messageAvatar.src = person.avatar;
-        messageElm.appendChild(messageAvatar);
+        
 
         const messageContainer = document.createElement('div');
         messageContainer.className = "flex-grow";
@@ -164,11 +182,47 @@ export default class MessagesView extends View {
         messageContent.innerText = message;
         messageContent.className = "bg-amber-200 h-min w-auto p-3 m-3 rounded-md drop-shadow-lg drop-shadow-amber-500 z-0";
 
-        messageContainer.appendChild(messageContent);
+        if (fromMe) {
+            messageElm.classList.add("justify-end");
+            messageContainer.appendChild(messageContent);
+            messageElm.appendChild(messageContent);
+            messageElm.appendChild(messageAvatar);
+            
+        } else {
+            messageContainer.appendChild(messageContent);
+            messageElm.appendChild(messageAvatar);
+            messageElm.appendChild(messageContent);
+        }
+       
 
-        messageElm.appendChild(messageContent);
+        
         
         this.#chatView.appendChild(messageElm);
+    }
+
+    initializeSendView() {
+        const messageForm = document.createElement('form');
+        messageForm.className = "h-auto";
+        messageForm.name = "message";
+        const messageArea = document.createElement('textarea');
+        messageArea.className = "m-5 resize-y text-lg block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
+        messageArea.id="message-area";
+        messageArea.addEventListener("keyup", e => {
+            e.preventDefault();
+            if (e.key === "Enter") {
+                console.log("enter");
+                this.addMessage(this.#currUser, Date.now(), messageArea.value, true);
+                this.#chatView.scrollTop = this.#chatView.scrollHeight;
+                messageForm.submit();
+                messageForm.reset();
+
+            }
+        })
+        messageForm.appendChild(messageArea);
+        
+
+
+        this.#sendView.appendChild(messageForm);
     }
 
 
