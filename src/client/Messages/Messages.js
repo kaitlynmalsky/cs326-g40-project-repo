c86b83836c7feef92f700e03cbff3d230f5f8e33
kaitlynmalsky <93287571+kaitlynmalsky@users.noreply.github.com>
2024-04-14 12:07:02 -0400

    message work

--- src/client/Messages/Messages.js
+++ src/client/Messages/Messages.js
@@ -1,30 +1,80 @@
 import View from '../View.js';
 
 export default class MessagesView extends View {
+    #messageList;
+    #curr_id;
+    #chatListElm;
+    
     constructor() {
         super();
+        this.curr_id = 0;
+        this.#messageList = [];
+        
     }
 
     async render() {
         const elm = document.createElement('header');
         elm.id = 'messages-view';
+        elm.className = "h-full flex justify-start items-start";
 
-        const splitScreenSection = document.createElement('section');
-        const splitGrid = document.createElement('div');
-        splitGrid.className = "grid h-screen grid-cols-2 gap-10";
         const col1 = document.createElement('div');
-        col1.innerHTML = "hi"
+
+        col1.className = "max-h-screen overflow-y-auto w-1/5 h-100 message-view-col bg-amber-500";
+
+
         const col2 = document.createElement('div');
+        col2.className = "max-h-screen overflow-y-auto message-view-col overflow-y-auto flex-grow";
         col2.innerHTML = "bye";
-        col1.classList.add("bg-amber-500");
 
+        //peopleElm = document.createElement()
 
-        elm.appendChild(splitScreenSection);
-        splitScreenSection.appendChild(splitGrid);
-        splitGrid.appendChild(col1);
-        splitGrid.appendChild(col2);
+        elm.appendChild(col1);
+        elm.appendChild(col2);
+
+        this.demoMessages();
+        console.log(this.#curr_id);
+
 
         return elm;
     }
+
+    demoMessages() {
+        const spiderman = {
+            name: "Spiderman",
+            avatar: "../images/spiderman.png"
+        }
+        const scoob = {
+            name: "Scoob",
+            avatar: '../images/Sooby_doo.png'
+        }
+        const nemo = {
+            name: "Nemo",
+            avatar: '../images/nemo.png'
+        }
+
+
+        this.addGroupChat([spiderman, scoob]);
+        this.addGroupChat([nemo]);
+        this.addGroupChat([spiderman, nemo, scoob]);
+
+        
+    }
+
+    renderAvatars() {
+
+    }
+
+    addGroupChat(people) {
+        // https://flowbite.com/docs/components/avatar/
+        this.#curr_id = this.#messageList.length;
+        this.#messageList.push({
+            id: this.curr_id,
+            people: people
+        });
+        this.curr_id++;
+    }
+
+
+
 
 }
