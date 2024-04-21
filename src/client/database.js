79018db96647b5d52707f54293daaf373b8c002b
anothergenz <git@angz.dev>
2024-04-21 15:03:20 -0400

    avatar config docs

--- src/client/database.js
+++ src/client/database.js
@@ -1,10 +1,20 @@
 /**
+ * Avatar configuration
+ * @typedef {object} AvatarConfig
+ * @property {number} bg
+ * @property {number} body
+ * @property {number} ears
+ * @property {number} hat
+ */
+
+/**
  * Input data to create a VillageLink User
  * @typedef {Object} CreateUserInput
  * @property {string} name The user's name
  * @property {string} username The user's username
  * @property {string} email The user's email
  * @property {string} avatar The user's avatar
+ * @property {AvatarConfig} avatarConfig The configuration for the user's avatar
  * @property {string} password The user's password
  */
 
@@ -15,6 +25,7 @@
  * @property {string} name The user's name
  * @property {string} password The user's password
  * @property {string} avatar The user's avatar
+ * @property {AvatarConfig} avatarConfig The configuration for the user's avatar
  * @property {string} _id PouchDB ID
  * @property {string} _rev PouchDB revision
  */
@@ -63,273 +74,272 @@
  * @property {string} avatar
  */
 
-
 /**
  * @class
  */
 class Database {
   #db;
 
-    constructor() {
-        this.#db = new PouchDB('villagelink');
+  constructor() {
+    this.#db = new PouchDB('villagelink');
+  }
+
+  /**
+   * Formats `pinID` into a database key
+   * @param {string} pinID The ID of the pin
+   * @returns {string}
+   */
+  #formatPinKey(pinID) {
+    return `pin_${pinID}`;
+  }
+
+  /**
+   * Formats `userID` into a database key
+   * @param {string} userID the ID of the user
+   * @returns {string}
+   */
+  #formatUserKey(userID) {
+    return `user_${userID}`;
+  }
+
+  /**
+   * Formats `userID` and `targetID` into a database key
+   * @param {string} userID
+   * @param {string} targetID
+   * @returns {string}
+   */
+  #formatConnectionKey(userID, targetID) {
+    return `connection_${userID}_${targetID}`;
+  }
+
+  /**
+   * Saves `userID` to `localStorage`
+   * @param {string} userID
+   */
+  setCurrentUserId(userID) {
+    localStorage.setItem('userID', userID);
+  }
+
+  /**
+   * Retrieves the current `userID` from `localStorage`
+   * @returns {string | null}
+   */
+  getCurrentUserID() {
+    return localStorage.getItem('userID');
+  }
+
+  /**
+   * Deletes the current `userId` from `localStorage`
+   */
+  deleteCurrentUserId() {
+    localStorage.removeItem('userID');
+  }
+
+  /**
+   * Creates a new pin
+   * @param {CreatePinInput} pinData Input data to create the pin
+   * @returns {Promise<Pin>} The created pin
+   */
+  async createPin(pinData) {
+    const pinID = self.crypto.randomUUID();
+
+    const pinDoc = {
+      _id: this.#formatPinKey(pinID),
+      pinID,
+      ...pinData,
+    };
+
+    const { id, rev, ok } = await this.#db.put(pinDoc);
+
+    if (!ok) {
+      console.error(`Failed to create ${pinID} (id=${id} , rev=${rev})`);
     }
 
-    /**
-     * Formats `pinID` into a database key
-     * @param {string} pinID The ID of the pin
-     * @returns {string}
-     */
-    #formatPinKey(pinID) {
-        return `pin_${pinID}`;
+    return {
+      ...pinDoc,
+      _id: id,
+      _rev: rev,
+    };
+  }
+
+  /**
+   * Gets pin information
+   * @param {string} pinID The ID of the pin to retrieve
+   * @returns {Promise<Pin>}
+   * @throws {Error}
+   */
+  async getPin(pinID) {
+    return this.#db.get(this.#formatPinKey(pinID));
+  }
+
+  /**
+   * Updates a pin
+   * @param {Pin} pin The pin to update
+   * @returns {Promise<Pin>}
+   */
+  async updatePin(pin) {
+    const { rev, ok, id } = await this.#db.put(pin);
+
+    if (!ok) {
+      console.error(`Failed to update ${pin.pinID} (id=${id} , rev=${rev})`);
     }
 
-    /**
-     * Formats `userID` into a database key
-     * @param {string} userID the ID of the user
-     * @returns {string}
-     */
-    #formatUserKey(userID) {
-        return `user_${userID}`;
+    pin._rev = rev;
+
+    return pin;
+  }
+
+  /**
+   * Delete a pin
+   * @param {Pin} pin The pin to delete
+   * @returns {Promise<PouchDB.Core.Response>}
+   */
+  async deletePin(pin) {
+    return this.#db.remove(pin);
+  }
+
+  /**
+   * Retrieves all pins
+   * @returns {Promise<Array<Pin>>}
+   */
+  async getAllPins() {
+    const pinsResult = await this.#db.allDocs({
+      include_docs: true,
+      startkey: 'pin',
+      endkey: `pin\ufff0`,
+    });
+
+    return pinsResult.rows.map((row) => row.doc);
+  }
+
+  /**
+   * Retrieves a user
+   * @param {string} userID The ID of the user to retrieve
+   * @returns {Promise<User>}
+   */
+  async getUser(userID) {
+    return this.#db.get(this.#formatUserKey(userID));
+  }
+
+  /**
+   * Creates a new connection
+   * @param {CreateVillageConnectionInput} connection Input data to create the connection
+   * @returns {Promise<VillageConnection>} The newly created connection
+   */
+  async createConnection(connection) {
+    const userID = this.getCurrentUserID();
+    const connectionKey = this.#formatConnectionKey(
+      userID,
+      connection.targetID,
+    );
+
+    const connectionDoc = {
+      ...connection,
+      _id: connectionKey,
+    };
+
+    const { id, rev, ok } = await this.#db.put(connectionDoc);
+
+    if (!ok) {
+      console.error(
+        `Failed to create ${connectionKey} (id=${id} , rev=${rev})`,
+      );
     }
 
-    /**
-     * Formats `userID` and `targetID` into a database key
-     * @param {string} userID
-     * @param {string} targetID
-     * @returns {string}
-     */
-    #formatConnectionKey(userID, targetID) {
-        return `connection_${userID}_${targetID}`;
+    return {
+      ...connectionDoc,
+      _rev: rev,
+    };
+  }
+
+  /**
+   * Retrieves connections for either the specified userID or for the current user
+   * @param {string} [userID]
+   * @returns {Promise<Array<VillageConnection>>}
+   */
+  async getConnections(userID) {
+    userID = userID || this.getCurrentUserID();
+    const connectionsFilterKey = `connection_${userID}`;
+    const connectionsResult = await this.#db.allDocs({
+      include_docs: true,
+      startkey: connectionsFilterKey,
+      endkey: `${connectionsFilterKey}\ufff0`,
+    });
+
+    return connectionsResult.rows.map((row) => row.doc);
+  }
+
+  /**
+   * Deletes the specified connection
+   * @param {VillageConnection} connection Connection to delete
+   * @returns {Promise<PouchDB.Core.Response>}
+   */
+  async deleteConnection(connection) {
+    return this.#db.remove(connection);
+  }
+
+  /**
+   * Retrieves user by email
+   * @param {string} email The email of the user to retrieve
+   * @returns {Promise<User>}
+   */
+  async getUserByEmail(email) {
+    const users = await this.#db.allDocs({
+      include_docs: true,
+      startkey: 'user',
+      endkey: `user\ufff0`,
+    });
+
+    const user = users.rows.find((row) => row.doc.email === email)?.doc;
+    return user || null;
+  }
+
+  async getUserByName(name) {
+    const users = await this.#db.allDocs({
+      include_docs: true,
+      startkey: 'user',
+      endkey: 'user\ufff0',
+    });
+
+    console.log(users);
+    const user = users.rows.filter((row) => row.doc.name === name);
+    return user;
+  }
+
+  /**
+   * Adds a new user
+   * @param {CreateUserInput} userData User data to create a new user
+   * @returns {Promise<User>} The created user
+   */
+  async addUser(userData) {
+    const existingUser = await this.getUserByEmail(userData.email);
+    if (existingUser) {
+      console.error(`An account with ${userData.email} already exists`);
+      return existingUser;
+    } else {
+      const userID = self.crypto.randomUUID();
+      const userDoc = {
+        _id: this.#formatUserKey(userID),
+        userID,
+        ...userData,
+      };
+
+      const { rev } = await this.#db.put(userDoc);
+
+      return {
+        ...userDoc,
+        _rev: rev,
+      };
     }
+  }
 
-    /**
-     * Saves `userID` to `localStorage`
-     * @param {string} userID
-     */
-    setCurrentUserId(userID) {
-        localStorage.setItem('userID', userID);
-    }
-
-    /**
-     * Retrieves the current `userID` from `localStorage`
-     * @returns {string | null}
-     */
-    getCurrentUserID() {
-        return localStorage.getItem('userID');
-    }
-
-    /**
-     * Deletes the current `userId` from `localStorage`
-     */
-    deleteCurrentUserId() {
-        localStorage.removeItem('userID');
-    }
-
-    /**
-     * Creates a new pin
-     * @param {CreatePinInput} pinData Input data to create the pin
-     * @returns {Promise<Pin>} The created pin
-     */
-    async createPin(pinData) {
-        const pinID = self.crypto.randomUUID();
-
-        const pinDoc = {
-            _id: this.#formatPinKey(pinID),
-            pinID,
-            ...pinData,
-        };
-
-        const { id, rev, ok } = await this.#db.put(pinDoc);
-
-        if (!ok) {
-            console.error(`Failed to create ${pinID} (id=${id} , rev=${rev})`);
-        }
-
-        return {
-            ...pinDoc,
-            _id: id,
-            _rev: rev,
-        };
-    }
-
-    /**
-     * Gets pin information
-     * @param {string} pinID The ID of the pin to retrieve
-     * @returns {Promise<Pin>}
-     * @throws {Error}
-     */
-    async getPin(pinID) {
-        return this.#db.get(this.#formatPinKey(pinID));
-    }
-
-    /**
-     * Updates a pin
-     * @param {Pin} pin The pin to update
-     * @returns {Promise<Pin>}
-     */
-    async updatePin(pin) {
-        const { rev, ok, id } = await this.#db.put(pin);
-
-        if (!ok) {
-            console.error(`Failed to update ${pin.pinID} (id=${id} , rev=${rev})`);
-        }
-
-        pin._rev = rev;
-
-        return pin;
-    }
-
-    /**
-     * Delete a pin
-     * @param {Pin} pin The pin to delete
-     * @returns {Promise<PouchDB.Core.Response>}
-     */
-    async deletePin(pin) {
-        return this.#db.remove(pin);
-    }
-
-    /**
-     * Retrieves all pins
-     * @returns {Promise<Array<Pin>>}
-     */
-    async getAllPins() {
-        const pinsResult = await this.#db.allDocs({
-            include_docs: true,
-            startkey: 'pin',
-            endkey: `pin\ufff0`,
-        });
-
-        return pinsResult.rows.map((row) => row.doc);
-    }
-
-    /**
-     * Retrieves a user
-     * @param {string} userID The ID of the user to retrieve
-     * @returns {Promise<User>}
-     */
-    async getUser(userID) {
-        return this.#db.get(this.#formatUserKey(userID));
-    }
-
-    /**
-     * Creates a new connection
-     * @param {CreateVillageConnectionInput} connection Input data to create the connection
-     * @returns {Promise<VillageConnection>} The newly created connection
-     */
-    async createConnection(connection) {
-        const userID = this.getCurrentUserID();
-        const connectionKey = this.#formatConnectionKey(
-            userID,
-            connection.targetID,
-        );
-
-        const connectionDoc = {
-            ...connection,
-            _id: connectionKey,
-        };
-
-        const { id, rev, ok } = await this.#db.put(connectionDoc);
-
-        if (!ok) {
-            console.error(
-                `Failed to create ${connectionKey} (id=${id} , rev=${rev})`,
-            );
-        }
-
-        return {
-            ...connectionDoc,
-            _rev: rev,
-        };
-    }
-
-    /**
-     * Retrieves connections for either the specified userID or for the current user
-     * @param {string} [userID]
-     * @returns {Promise<Array<VillageConnection>>}
-     */
-    async getConnections(userID) {
-        userID = userID || this.getCurrentUserID();
-        const connectionsFilterKey = `connection_${userID}`;
-        const connectionsResult = await this.#db.allDocs({
-            include_docs: true,
-            startkey: connectionsFilterKey,
-            endkey: `${connectionsFilterKey}\ufff0`,
-        });
-
-        return connectionsResult.rows.map((row) => row.doc);
-    }
-
-    /**
-     * Deletes the specified connection
-     * @param {VillageConnection} connection Connection to delete
-     * @returns {Promise<PouchDB.Core.Response>}
-     */
-    async deleteConnection(connection) {
-        return this.#db.remove(connection);
-    }
-
-    /**
-     * Retrieves user by email
-     * @param {string} email The email of the user to retrieve
-     * @returns {Promise<User>}
-     */
-    async getUserByEmail(email) {
-        const users = await this.#db.allDocs({
-            include_docs: true,
-            startkey: 'user',
-            endkey: `user\ufff0`,
-        });
-
-        const user = users.rows.find((row) => row.doc.email === email) ?.doc;
-        return user || null;
-    }
-
-    async getUserByName(name) {
-        const users = await this.#db.allDocs({
-            include_docs: true,
-            startkey: 'user',
-            endkey: 'user\ufff0',
-        });
-
-        console.log(users);
-        const user = users.rows.filter((row) => row.doc.name === name);
-        return user;
-    }
-
-    /**
-     * Adds a new user
-     * @param {CreateUserInput} userData User data to create a new user
-     * @returns {Promise<User>} The created user
-     */
-    async addUser(userData) {
-        const existingUser = await this.getUserByEmail(userData.email)
-        if (existingUser) {
-            console.error(`An account with ${userData.email} already exists`);
-            return existingUser;
-        } else {
-            const userID = self.crypto.randomUUID();
-            const userDoc = {
-                _id: this.#formatUserKey(userID),
-                userID,
-                ...userData,
-            };
-
-            const { rev } = await this.#db.put(userDoc);
-
-            return {
-                ...userDoc,
-                _rev: rev,
-            };
-        }
-    }
-
-    /**
-     * Adds a group chat.
-     * @param {number} id 
-     */
-    async addGroupChat(id) {
-      // const existingGroupChat = await
-    }
+  /**
+   * Adds a group chat.
+   * @param {number} id
+   */
+  async addGroupChat(id) {
+    // const existingGroupChat = await
+  }
 }
 
 const dbInstance = new Database();
