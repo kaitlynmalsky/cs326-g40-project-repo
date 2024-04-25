00980524705e0dd9485f26251a15907eeb58b646
Sreyas2401 <srajasekharu@umass.edu>
2024-04-24 22:23:33 -0400

    JSDocs Part2

--- src/client/mock.js
+++ src/client/mock.js
@@ -34,6 +34,10 @@ function testUser() {
     return user;
 }
 
+/**
+ * Creates mock connections and sub-connection between any one element and the rest.
+ * @returns {string[]}
+ */
 export async function mockUsers() {
     const ids = [];
     const currentUserID = dbInstance.getCurrentUserID();
@@ -74,47 +78,66 @@ export async function mockUsers() {
     return ids;
 }
 
-// Generate a random date within a range
+/**
+ * Generates a random date within a given range.
+ * @param {Date} start - The start date of the range.
+ * @param {Date} end - The end date of the range.
+ * @returns {Date} A random date between the start and end dates.
+ */
 function getRandomDate(start, end) {
     return new Date(
         start.getTime() + Math.random() * (end.getTime() - start.getTime()),
     );
 }
 
-// Format a date to ISO-8601 format (YYYY-MM-DDTHH:MM:SSZ)
+/**
+ * Formats a date to the ISO-8601 format (YYYY-MM-DDTHH:MM:SSZ).
+ * @param {Date} date - The date to be formatted.
+ * @returns {string} The ISO-8601 formatted date string.
+ */
+
 function formatDateToISO(date) {
     return date.toISOString();
 }
 
-// Generate random start time and end time
+/**
+ * Generates random start time and end time within a range of 7 days from today.
+ * @returns {{ startTime: string, endTime: string }} An object containing the generated start and end times in ISO-8601 format.
+ */
+
 function generateRandomTimeRange() {
-    const startDate = new Date(); // Today's date
+    const startDate = new Date();
     const endDate = new Date();
-    endDate.setDate(startDate.getDate() + 7); // End date is 7 days from today
+    endDate.setDate(startDate.getDate() + 7);
 
     const startTime = getRandomDate(startDate, endDate);
     const endTime = getRandomDate(startTime, endDate);
 
-    // Format dates to ISO-8601 format
     const startTimeISO = formatDateToISO(startTime);
     const endTimeISO = formatDateToISO(endTime);
 
     return { startTime: startTimeISO, endTime: endTimeISO };
 }
 
+/**
+ * Generates random coordinates within a bounding box.
+ * @returns {number[]} An array containing the generated random coordinates [x, y].
+ */
 function generateRandomCoords() {
     const x1 = 42.41319;
     const y1 = -72.54448;
     const x2 = 42.36495;
     const y2 = -72.4967;
 
-    // Generate random x and y coordinates within the bounding box range
     const randomX = Math.random() * (x2 - x1) + x1;
     const randomY = Math.random() * (y2 - y1) + y1;
 
     return [randomX, randomY];
 }
 
+/**
+ * Creates mock pins based on user data 
+ */
 export async function mockPins() {
     const pins = (await dbInstance.getUpcomingPins()).length;
     if (pins < 15) {
