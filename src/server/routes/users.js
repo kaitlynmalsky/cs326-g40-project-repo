af22d672216adcd032b0100248c2d83938d8764e
Sreyas2401 <srajasekharu@umass.edu>
2024-04-30 18:58:21 -0400

    basic user routing

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getUser, getUserByEmail, updateUser } from '../database.js';
+import { getUser, getUserByEmail, updateUser, createConnection, getConnections, getConnection } from '../database.js';
 
 const userRouter = Router();
 
@@ -24,9 +24,75 @@ userRouter.get('/', async (req, res) => {
   return res.status(200).json(user);
 });
 
-userRouter.post('/:userId/connections', async (req, res) => {});
-userRouter.get('/:userID/connections', async (req, res) => {});
-userRouter.delete('/:userID/connections/:targetID', async (req, res) => {});
+/**
+ * Create a bi-directional connection between userID and targetID
+ */
+userRouter.post('/:userId/connections/', async (req, res) => {
+  const userID = req.params.userId;
+  const targetData = req.body;
+
+  if (!targetData.targetID) {
+    return res.status(400).json({ error: 'Missing targetID in request body' });
+  }
+
+  try {
+    const results = await Promise.all([
+      createConnection({
+        userID: userID,
+        targetID: targetData.targetID
+      }),
+      createConnection({
+        userID: targetData.targetID,
+        targetID: userID
+      }),
+    ]);
+
+    res.status(201).json(results);
+
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create connections' });
+  }
+});
+
+/**
+ * Get all connections of `userID`
+ */
+userRouter.get('/:userID/connections', async (req, res) => {
+  const userID = req.params.userID;
+  const userData = req.body;
+
+  if (userID !== userData.userID) {
+    return res.status(400).json({ error: 'Wrong userID provided in get all connections data' });
+  }
+
+  try {
+    const connections = await getConnections(userID);
+    res.status(201).json(connections);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to get all connections of user' })
+  }
+
+});
+
+/**
+ * Delete a connection between userID and targetID
+ */
+userRouter.delete('/:userID/connections/:targetID', async (req, res) => {
+  const userID = req.params.userID;
+  const targetID = req.params.targetID;
+  const data = req.body;
+
+  if (userID !== data.userID || targetID !== data.targetID) {
+    return res.status(400).json({ error: 'Wrong userID or targetID provided in request body' });
+  }
+
+  try {
+    const connection = await getConnection(userID, targetID);
+    res.status(201).json(connection);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to get connection' });
+  }
+});
 
 /**
  * Get a user by `userID`
