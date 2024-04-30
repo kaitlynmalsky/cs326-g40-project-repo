bc6cd63a948682771792fb1dec97d794fa3b9c20
kaitlynmalsky <93287571+kaitlynmalsky@users.noreply.github.com>
2024-04-30 19:15:51 -0400

    message routing (very basic)

--- src/server/routes/messages.js
+++ src/server/routes/messages.js
@@ -1,5 +1,18 @@
 import Router from 'express';
+import { getAllGroupChats, getGroupById } from '../database.js';
 
 const messagesRouter = Router();
+
+/**
+ * Get a group chat by `gcID`
+ */
+messagesRouter.get('/:gcID', async(req, res) => {
+    const gcID = req.params.gcID;
+    const gc = await getGroupById(Number(gcID));
+    if (!gc) {
+        return res.status(404).end();
+    }
+    return res.status(200).json(gc);
+});
 
 export default messagesRouter;
