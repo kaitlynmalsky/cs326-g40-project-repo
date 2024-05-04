1c24984c4db1d30f9d8fc1f74fbba18099d9cdf0
anothergenz <git@angz.dev>
2024-05-04 14:16:17 -0400

    add cors

--- src/server/index.js
+++ src/server/index.js
@@ -7,8 +7,10 @@ import { createUser, db, getUserByEmail } from './database.js';
 import session, { Session } from 'express-session';
 import PouchDBSessionStore from './session.js';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(
